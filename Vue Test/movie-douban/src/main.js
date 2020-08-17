664f734b8d9550bc3be1b7083e04d8af98d92e02
wupj <wdotl@outlook.com>
2020-08-17 21:56:14 +0800

    增加vue-awesome-swiper依赖包

--- Vue Test/movie-douban/src/main.js
+++ Vue Test/movie-douban/src/main.js
@@ -4,11 +4,14 @@ import store from "./store";
 import router from "./router";
 import i18n from "./locales";
 import Element from "element-ui";
+import VueAwesomeSwiper from "vue-awesome-swiper";
 
 import "element-ui/lib/theme-chalk/index.css";
+import "swiper/swiper-bundle.css";
 import "@/assets/styles/index.scss";
 
 Vue.use(Element);
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 
